626fadd32cab121a27f13386df3316ce0418b10b
SabinCacho-MMDC <lr.smcacho@mmdc.mcl.edu.ph>
2025-10-24 04:23:54 +0800

    fixed profile picture display
    fixed profile pictures failing to display in about-me section

--- scripts/script.js
+++ scripts/script.js
@@ -34,7 +34,7 @@ function changeContent(name){
     switch(name){
         case "Sabin":
             aboutMeContent.textContent = "Hello! I'm Sabin Miguel B. Cacho, and this is my portfolio. I am a Third Year Software Development student at Mapua Malayan Digital College. I have been studying Software Dev't for around 5 years now—having started during Senior Highschool—until now. My passions lie in the logical, critical nature of programming, where everything has a particular purpose and meaning. Coding, to me, feels like one big, strange puzzle—one where I start with the final image, but none of the pieces; where I am supposed to find or create my own pieces to end up at the final image I began with. "
-            aboutMeImg.src = "/styles/img/CACHO_ID_Picture.png"
+            aboutMeImg.src = "styles/img/CACHO_ID_Picture.png"
             info = [
                 "0998-510-3116",
                 "lr.smcacho@mmdc.mcl.edu.ph",
@@ -47,7 +47,7 @@ function changeContent(name){
             break;
         case "Gio":
             aboutMeContent.textContent = "Hi there! I'm Giancarlo M. Jamandri, and this is my portfolio. I am a 2nd year College student taking up IT specializing in Cybersecurity and Networks at Mapua Malayan Digital College. I am Sophomore but this is my first year taking the Cybersecurity specialization, in my first year I was focused on general IT at Southville International School and Colleges. I may be an IT student but a passion of mine growing up has always been of the arts in terms of drawing and as well as video editing, a sub product of doing video editing is also a bit of voice acting for personal projects.";
-            aboutMeImg.src = "/styles/img/JAMANDRI_ID_Picture.jpg"
+            aboutMeImg.src = "styles/img/JAMANDRI_ID_Picture.jpg"
             info = [
                 "+63-283-555-0962",
                 "geodude@gmail.com",
